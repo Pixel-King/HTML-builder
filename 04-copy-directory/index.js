52811fff6f388bbb0d9bc9dea325da4f9ba2491d
Yaraslau Borodavkin <y.borodavkin2011@gmail.com>
2022-05-25 23:40:22 +0300

    fix copy folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,23 +2,28 @@ const fs = require('fs');
 const path = require('path');
 
 (function copyDir() {
-    const baseFolder = path.join(__dirname, 'files');
-    fs.mkdir(path.join(__dirname, 'copy-files'),{recursive: true}, err =>{
-        if (err) throw err;
-    });
-    fs.readdir(
-        baseFolder,
-        {withFileTypes: true},
-        (err, files) => {
-            if(err) throw err;
-             files.forEach( file => {
-                if(file.isFile()){
-                    const baseFile = path.resolve(__dirname, 'files', file.name);
-                    const copyFile = path.resolve(__dirname, 'copy-files', file.name);
-                    fs.copyFile(baseFile, copyFile, err => {
-                        if (err) throw err;
-                    });
-                }
-            }); 
-        });
+    const baseFolder = path.resolve(__dirname, 'files');
+    const copyFolder = path.resolve(__dirname, 'copy-files');
+    try {
+         fs.rm(copyFolder,{recursive: true, force: true}, ()=>{
+                fs.mkdir(path.join(__dirname, 'copy-files'),() => {
+                    fs.readdir(
+                        baseFolder,
+                        {withFileTypes: true},
+                        (err, files) => {
+                            if(err) throw err;
+                            files.forEach( file => {
+                                if(file.isFile()){
+                                    fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'copy-files', file.name), err => {
+                                        if (err) throw err;
+                                    });
+                                }
+                            }); 
+                        });
+                });
+            });
+    } catch(err){
+            console.log(err)
+        }
 })();
+
